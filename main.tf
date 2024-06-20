5dcf8d2e01fb0d898d13d7e2a1a1c75524d3530d
Ronaldo Castro Milán <43684050+rocamil85@users.noreply.github.com>
2024-06-20 18:55:33 -0400

    insert a dataflow

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+resource "google_dataflow_job" "big_data_job" {
+  name              = "dataflow-job"
+  template_gcs_path = "gs://my-bucket/templates/template_file"
+  temp_gcs_location = "gs://my-bucket/tmp_dir"
+  parameters = {
+    foo = "bar"
+    baz = "qux"
+  }
+}
+
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = "ubuntu-os-cloud" 
